e03ce7b911fd88a28d40fb1b170047f7740f8d86
JWL <j.lebert4402@gmail.com>
2023-06-25 19:35:54 -0400

    made a simple replacement for one word

--- main.c
+++ main.c
@@ -5,8 +5,13 @@ int main(int argc, char** argv) {
     int i;
     char c;
 
-    for (i = 0; i < argc; i++) {
-        printf("%d\n", strlen(*(argv + i)));
+    for (i = 1; i < argc; i++) {
+        char* str = *(argv + i);
+        if (!strcmp(str, "your")) {
+            fputs("thy ", stdout);
+        } else {
+            printf("%s ", str);
+        }
     }
 
     return 0;
